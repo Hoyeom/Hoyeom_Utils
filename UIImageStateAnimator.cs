2c0dd761da0c7c89400bbcfdf81ddc09d19692e9
Hoyeom <korehdrb@gmail.com>
2023-05-30 14:44:46 +0900

    Add files via upload

---
+++ UIImageStateAnimator.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace Main.Utils.Component
+{
+    
+    /// <summary>
+    /// https://github.com/Hoyeom/Hoyeom_Utils
+    /// </summary>
+
+    [RequireComponent(typeof(Image))]
+    public class UIStateStateAnimator : MonoBehaviour
+    {
+        [SerializeField] private Image image;
+        [field:SerializeField] private int StateIndex { get; set; }
+        [field:SerializeField] public int MillisecondsDelay { get; set; } = 200;
+        
+        public bool unscaledTime;
+        public SpriteAnimatorState[] states;
+        private CancellationTokenSource _cts;
+
+        [Serializable]
+        public class SpriteAnimatorState
+        {
+            public Sprite[] sprites;
+            public bool hasExitTime;
+            public bool loop;
+        }
+
+        private void Awake()
+        {
+            if (TryGetComponent<Image>(out var component))
+                image = component;
+
+            PlayRoutine();
+        }
+
+        public UIStateStateAnimator SetState(int index)
+        {
+            StateIndex = index;
+            return this;
+        }
+
+        public UIStateStateAnimator SetSpeed(int millisecondsDelay)
+        {
+            MillisecondsDelay = millisecondsDelay;
+            return this;
+        }
+
+        public UIStateStateAnimator Stop()
+        {
+            _cts?.Cancel();
+            _cts?.Dispose();
+            return this;
+        }
+
+        public async UniTaskVoid PlayRoutine(int millisecondsDelay = 200, int stateIndex = 0)
+        {
+            if(states == null)
+                return;
+            
+            var stateLength = states.Length;
+            
+            if(stateLength == 0)
+                return;
+            
+            MillisecondsDelay = millisecondsDelay;
+            StateIndex = stateIndex;
+            
+            _cts?.Cancel();
+            _cts?.Dispose();
+            
+            _cts = new CancellationTokenSource();
+            
+            var token = _cts.Token;
+
+            while (true)
+            {
+                StateIndex = Mathf.Clamp(StateIndex, 0, stateLength - 1);
+                
+                var state = states[StateIndex];
+
+                var prevStateIndex = StateIndex;
+                var hasExitTime = state.hasExitTime;
+
+                var spriteLength = state.sprites.Length;
+                var loop = state.loop;
+
+                int index = 0;
+
+                Func<int, int> calculator = loop ? LoopCalculator : Calculator;
+
+                while (true)
+                {
+                    if (StateIndex != prevStateIndex)
+                    {
+                        if (!hasExitTime || index == spriteLength - 1)
+                            break;
+                    }
+                    
+                    image.sprite = state.sprites[index];
+                    index = calculator.Invoke(index);
+                    await UniTask.Delay(MillisecondsDelay, unscaledTime, PlayerLoopTiming.Update, token);
+                }
+                
+                int LoopCalculator(int value) => (value + 1) % spriteLength;
+                int Calculator(int value) => Mathf.Clamp(value + 1, 0, spriteLength - 1);
+            }
+        }
+
+
+    }
+}
